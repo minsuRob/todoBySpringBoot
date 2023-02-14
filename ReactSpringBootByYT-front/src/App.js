25cd789c12d53050c91934dce5ce52a82063fcd2
minsu.rob <minsu.rob@gmail.com>
2023-02-14 21:23:49 +0900

    feat : input의 값을 바꾸는 함수 추가

--- ReactSpringBootByYT-front/src/App.js
+++ ReactSpringBootByYT-front/src/App.js
@@ -6,9 +6,44 @@ import axios from "axios";
 
 function App() {
 
+  const baseUrl = "http://localhost:8081"
+
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    getTodos();
+  }, [])
+
+  async function getTodos() {
+    await axios
+    .get(baseUrl + "/todo")
+    .then((response)=> {
+        console.log(response)
+        console.log(response.data)
+    })
+    .catch((error) =>{
+      console.error(error);
+    })
+
+
+  }
+
+  function changeText(e) {
+    e.preventDefault();
+    setInput(e.target.value);
+    
+  }
+  
   return (
     <div className="App">
-      <h1>1</h1>
+      <h1>Todo List</h1>
+      <form>
+        <label>
+          Todo &nbsp;
+          <input type="text" required={true} value={input} onChange={changeText}/>
+        </label>
+        <input type="submit" value="Create"/>
+      </form>
     </div>
   );
 }
