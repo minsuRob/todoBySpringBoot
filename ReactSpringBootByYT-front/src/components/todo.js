000f57c557b387384f24f23aed11dd1ff8771976
minsu.rob <minsu.rob@gmail.com>
2023-02-15 01:13:35 +0900

    feat: 컴포넌트 분리

--- ReactSpringBootByYT-front/src/components/todo.js
+++ ReactSpringBootByYT-front/src/components/todo.js
@@ -1,5 +1,15 @@
 function Todo(props) {
-
+return (
+    <div className="todo" key={props.todo.id}>
+        <h3>
+        <label className = {props.todo.completed ? "completed" : null}
+            onClick={props.handleClick}>
+            {props.todo.todoName}
+        </label>
+        <label onClick={props.handleDelete}>&nbsp;&nbsp;❌</label>
+        </h3>
+    </div>
+)
 }
 
 export default Todo;
